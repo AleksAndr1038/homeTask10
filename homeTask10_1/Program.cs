4b8511af958b0b637d9605cc3490f9af98774048
AleksAndr1038 <107847302+AleksAndr1038@users.noreply.github.com>
2022-06-21 18:51:34 +0300

    Add files via upload

---
+++ homeTask10_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using static System.Console;
+
+namespace homeTask10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Text;
+            string word;
+            int count = 0;
+
+            string filePath = "test.txt";
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.Unicode))
+                {
+                    Text = sr.ReadToEnd();
+                }
+            }
+
+         string[] textArray = Text.Split(' ',  '-', '.', ',');
+            WriteLine("Введите слово");
+            word = Convert.ToString(ReadLine());
+
+            for (int i = 0; i < textArray.Length - 1; i++)
+            {
+                if (textArray[i].Equals(word))
+                {
+                    count++;
+                }
+            }
+
+            WriteLine($"Данное слово встречается {count} раз");
+        }
+    }
+}
